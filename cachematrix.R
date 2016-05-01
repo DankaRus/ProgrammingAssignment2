cface4a45e209a8b6609539bc53f329f7159ce75
DankaRus <galdina.daria@gmail.com>
2016-05-01 14:26:33 +0300

    [new] done!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <-function(x=matrix(numeric(),nrow=0,ncol=0)){
+  inv <- NULL
+  oldx <- matrix(numeric(),nrow=0,ncol=0)
+  compare <- function(){
+    flag <<- is.matrix(x) && is.matrix(oldx) && dim(x) == dim(oldx) && all(x == oldx) 
+    oldx<<-x
+    flag
+    }
+  set <- function(y){
+    oldx <<- x 
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set=set, get=get, 
+       setinv=setinv,
+       getinv=getinv,compare=compare)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...){
+  inv <- x$getinv()
+  bulka<-x$compare()
+  if(!is.null(inv) && bulka)
+  {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
